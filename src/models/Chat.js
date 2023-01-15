f0e70f869d4b48eaf74d68e4b74d8ac838bd8c41
ivansansao <aivanplus@gmail.com>
2023-01-15 19:31:15 -0300

    Styling chat itens

--- src/models/Chat.js
+++ src/models/Chat.js
@@ -2,7 +2,10 @@ class Chat {
     constructor() {
 
         this.addListener()
+
+        // this.addHtmlChatItem({ login: 'Renata', message: 'Oiii' })
     }
+
     addListener() {
 
         let el
@@ -21,20 +24,20 @@ class Chat {
 
         this.clientSend({
             command: 'general-message',
-            user: player.login,
+            login: player.login,
             message: message.innerText
         })
 
     }
 
     onMessageReceived(data) {
-        this.addHtmlChatItem({ user: data.user, message: data.message })
+        this.addHtmlChatItem({ login: data.login, message: data.message })
     }
     send() {
 
         this.clientSend({
             command: 'general-message',
-            user: player.login,
+            login: player.login,
             message: document.getElementById('inputMessage').value
         })
 
@@ -43,12 +46,41 @@ class Chat {
         client.send(JSON.stringify(data))
     }
 
-    addHtmlChatItem({ user, message }) {
+    addHtmlChatItem({ login, message }) {
 
         const chatItens = document.getElementById('chatItens')
-        chatItens.innerHTML += `<div class="chat-item"><b>${user}</b>: <span>${message}</span></div>`
+        chatItens.innerHTML += this.getDivChatItem({ login, message })
         chatItens.scrollTop = chatItens.scrollHeight
 
+    }
+    getDivChatItem({ login, message }) {
+
+        let div
+        let cls = 'chat-item'
+        let userSide
+        const firstChar = login[0]
+
+        if (login == player.login) {
+            cls = 'chat-item'
+            userSide = '1'
+        } else {
+            cls = 'chat-item chat-item-other'
+            userSide = '2'
+        }
+
+        div = `
+            <div class="chat-item-flex${userSide}">
+                <div class="${cls}">
+                    <div class="chat-item-user">${firstChar}</div>
+                    <div class="chat-item-message${userSide}">
+                        <div class="chat-item-message-title"><b>${login}</b></div>
+                        <div class="chat-item-message-content">${message}</div>
+                    </div>
+                </div>
+            </div>
+        `
+
+        return div
     }
 
 }
