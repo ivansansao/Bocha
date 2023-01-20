e14cfd53d0de2f582708f46d590b244d0fc38d97
ivansansao <aivanplus@gmail.com>
2023-01-19 23:20:51 -0300

    'p' key was pausing the game for debug lol, obvisously i have removed its, because when chating it was pausing the game

--- src/models/Keyboard.js
+++ src/models/Keyboard.js
@@ -1,11 +1,2 @@
 function keyPressed() {
-    if (key == 'p') {
-        console.log('Parado!');
-        if (running) {
-            noLoop();
-        } else {
-            loop();
-        }
-        running = !running;
-    }
 }
